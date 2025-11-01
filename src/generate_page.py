d12babf95a08cc0125080bed6f23f34e93989e90
simoroberts <simoroberts1@gmail.com>
2025-11-01 07:48:27 -0400

    static page generation recursively

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,4 @@
+import os
 from block_markdown import markdown_to_blocks, markdown_to_html_node
 
 def extract_title(markdown:str) -> str:
@@ -21,6 +22,16 @@ def generate_page(from_path:str, template_path:str, dest_path:str) -> None:
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
-
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
     with open(dest_path, "w") as fd:
         fd.write(template)
+
+def generate_pages_recursive(dir_path_content:str, template_path:str, dest_dir_path:str) -> None:
+    for file in os.listdir(dir_path_content):
+        filepath = os.path.join(dir_path_content, file)
+        if os.path.isdir(filepath):
+            generate_pages_recursive(filepath, template_path, os.path.join(dest_dir_path, file))
+        else:
+            generate_page(filepath, template_path, os.path.join(dest_dir_path, "index.html"))
--- src/main.py
+++ src/main.py
@@ -3,10 +3,15 @@ import os
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode
 from copy_files import build
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     build(os.path.abspath("./static/"), os.path.abspath("./public/"))
-    generate_page("./content/index.md", "./template.html", os.path.join("./public", "index.html"))
+    generate_pages_recursive("./content","./template.html", "./public" )
+    # generate_page("./content/index.md", "./template.html", os.path.join("./public", "index.html"))
+    # generate_page("./content/blog/glorfindel/index.md", "./template.html", os.path.join("./public/blog/glorfindel", "index.html"))
+    # generate_page("./content/blog/tom/index.md", "./template.html", os.path.join("./public/blog/tom", "index.html"))
+    # generate_page("./content/blog/majesty/index.md", "./template.html", os.path.join("./public/blog/majesty", "index.html"))
+    # generate_page("./content/contact/index.md", "./template.html", os.path.join("./public/contact", "index.html"))
 if __name__ == "__main__":
     main()
